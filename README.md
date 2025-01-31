25ff172224a182acac47ccaf66fd5a002a300533
Ben Butterworth <sw19145@bristol.ac.uk>
2025-01-31 14:07:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RSS Reader
+> a small utility for parsing RSS files from various scientific publications
---
+++ src/rssreader.py
@@ -0,0 +1,7 @@
+import json
+import feedparser
+import os
+
+# get list of URLs
+
+# parse RSS feeds from URLs
