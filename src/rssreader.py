adf677cfdbd80e60c5794808d698adb9cc0aeba9
Ben Butterworth <sw19145@bristol.ac.uk>
2025-02-03 12:13:13 +0000

    add keyword search, title printing

--- src/rssreader.py
+++ src/rssreader.py
@@ -13,16 +13,22 @@ def get_feed_info(feed):
     postsList = []
 
     for post in posts: 
-        postinfo = dict() 
+        postinfo = dict()
         try: 
             postinfo["title"] = post.title 
             postinfo["link"] = post.link 
-            postinfo["author"] = post.author 
-            postinfo["time_published"] = post.published 
-            postinfo["tags"] = [tag.term for tag in post.tags] 
-            postinfo["authors"] = [author.name for author in post.authors] 
             postinfo["summary"] = post.summary 
+            # postinfo["time_published"] = post.published 
+            # postinfo["authors"] = [author.name for author in post.authors] 
         except e: 
+            Exception("essential information missing")
+        try:
+            postinfo["author"] = post.author 
+        except:
+            pass
+        try:
+            postinfo["tags"] = [tag.term for tag in post.tags] 
+        except:
             pass
         postsList.append(postinfo) 
         
@@ -41,6 +47,40 @@ allposts = []
 # parse RSS feeds from URLs
 for url in feeds:
     feed = feedparser.parse(url)
+    feedInfo = get_feed_info(feed)
     # do everything else here
-    data = json.dumps(get_feed_info(feed))
-    allposts.append(data)
+    data = json.dumps(feedInfo)
+    allposts.append(feedInfo)
+
+def list_post_titles(feedinfo):
+    # feedinfo as dict 
+    posts = feedinfo['posts']
+    for i, post in enumerate(posts):
+        print(i, post["title"])
+    return 0
+
+keywords = [
+    "quantum",
+    "information",
+    "computing",
+    "entropy"
+]
+
+def keyword_search(feed):
+    # seach for mentions of keyword in title or summary of an article
+    posts = feed["posts"]
+    goodPosts = []
+    for post in posts:
+        text = post["title"] + " " + post["summary"]
+        score = 0
+        for keyword in keywords:
+            if keyword in text:
+                score += 1
+        if score != 0:
+            goodPosts.append(post)
+    return goodPosts
+            
+
+list_post_titles(allposts[0])
+
+print(keyword_search(allposts[0]))
